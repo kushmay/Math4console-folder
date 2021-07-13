fb65e1f0aa31bfb273bbd3909573e1bb4a2250cb
kushmay <81627608+kushmay@users.noreply.github.com>
2021-07-13 17:48:59 +0530

    Add files via upload

---
+++ game_page_login.js
@@ -0,0 +1,10 @@
+function addUser() {
+player1=document.getElementById("player1_name").value;
+player2=document.getElementById("player2_name").value;
+console.log(player1);
+console.log(player2);
+localStorage.setItem("player1" ,player1);
+localStorage.setItem("player2", player2);
+
+window.location="quiz_game_page.html";
+}
